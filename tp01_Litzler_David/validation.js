83798cad486106cca19f159cde9f38692bd53390
LITZLER David <litzlerdavid@gmail.com>
2020-09-10 17:36:24 +0200

    Final commit

---
+++ tp01_Litzler_David/validation.js
@@ -0,0 +1,16 @@
+$('form').submit(function(evt) {
+  if (($('#inputPassword').val() === $('#inputConfirmPassword').val())) {
+    if($('#inputPassword').val() === "" && $('#inputConfirmPassword').val()=== ""){     
+      alert('Veuillez renseigner un mot de passe');
+    }
+    else{
+      alert('Les mots de passe correspondent');
+      evt.preventDefault();
+    }
+    
+  }
+  else{
+    alert('Les mots de passe ne correspondent pas');
+  }
+})
+	
